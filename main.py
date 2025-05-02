7f7ac59fa4157d85d6632f1baca0081b23e8c883
rMaxiQp <zq2@illinois.edu>
2025-05-01 23:10:16 -0500

    Add plotting

---
+++ main.py
@@ -0,0 +1,116 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+import os
+
+
+def load_datapoints(dir: str):
+    """
+    Load data points from a directory containing JSON files.
+    Each JSON file should contain a list of data points.
+    """
+    datapoints = {}
+    for filename in os.listdir(dir):
+        if filename.endswith('.json'):
+            site = filename.replace('http:__localhost:8080_', '').replace('.json', '')
+            with open(os.path.join(dir, filename), 'r') as f:
+                data = json.load(f)
+                datapoints[site] = data
+    return datapoints
+
+
+def intersect(a: dict, b: dict):
+    """
+    Find the intersection by keys of two dictionary.
+    """
+    c = {}
+    for key in a:
+        if key not in b:
+            continue
+
+        c[key] = [a[key], b[key]]
+    return c
+
+def datapoint_to_array(data: dict):
+    """
+    Convert a dictionary of data points to a numpy array.
+    """
+    keys = sorted(list(data.keys()), key=int)
+    values = [data[key] for key in keys]
+    return np.array(values)
+
+def linear_plot(data: dict, site: str):
+    """
+    Plot the data points.
+    """
+    plt.figure(figsize=(10, 6))
+    for key, value in data.items():
+        plt.plot(value, label=key)
+    plt.title(f"Data points for {site}")
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    plt.legend()
+    plt.show()
+
+def percent_plot(data: dict, title: str):
+    """
+    Plot the data points as percentages.
+    """
+
+    offset = 0.125
+    width = 0.25
+    ticks = [60, 3600, 21600, 86400, 604800, 31622400]
+    x = np.arange(len(ticks))
+    fig, ax = plt.subplots(layout='constrained')
+    fig.set_figwidth(10)
+
+    baseline = ax.bar(x - offset, data['baseline'], width, label='baseline')
+    ax.bar_label(baseline, padding=3)
+    improve = ax.bar(x + offset, data['improve'], width, label='improve')
+    ax.bar_label(improve, padding=3)
+
+    ax.set_title(f"{title} (in %)")
+    ax.set_xlabel('Time (seconds)')
+    ax.set_ylabel('Percentage')
+    ax.legend(loc='best')
+    ax.set_xticks(range(len(ticks)))
+    ax.set_xticklabels(ticks)
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    # Load data points from the specified directory
+    datapoints = load_datapoints('data')
+    new_datapoints = load_datapoints('log')
+
+    print(f"Loaded {len(datapoints)} data points from 'data' directory.")
+    print(f"Loaded {len(new_datapoints)} data points from 'new_data' directory.")
+    same_sites = intersect(datapoints, new_datapoints)
+    print(f"Found {len(same_sites)} same sites.")
+
+
+    for site, data in same_sites.items():
+        print(f"Site: {site}")
+        old, new = data
+        # print(old)
+        updated = {}
+        for key in old.keys():
+            print(f"Key: {key}")
+            boarderline_old = datapoint_to_array(old[key]['ClassicLoadTester'])
+            boarderline_new = datapoint_to_array(new[key]['ClassicLoadTester'])
+            boarderline = np.mean(np.stack((boarderline_old, boarderline_new)), axis=0)
+            baseline = datapoint_to_array(old[key]['CacheV2LoadTester'])
+            improve = datapoint_to_array(new[key]['CacheV2LoadTester'])
+
+            baseline_percent = (boarderline_old - baseline) / boarderline_old * 100
+            improve_percent = (boarderline_new - improve) / boarderline_new * 100
+            percent_plot({
+                'baseline': baseline_percent,
+                'improve': improve_percent
+            }, f"{site} {key}")
+
+    # for site, data in same_sites.items():
+    #     print(f"Site: {site}")
+        # print(f"Data points from 'data': {data[0]}")
+        # print(f"Data points from 'new_data': {data[1]}")
